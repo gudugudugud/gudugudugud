24e708a519b2d20d6b8ceb28a11890657a1db839
gudugudugud <102131981+gudugudugud@users.noreply.github.com>
2022-03-22 10:09:51 +0530

    Add files via upload

---
+++ Prg9.java
@@ -0,0 +1,19 @@
+class Prg9 
+{
+	public static void main(String[] args) 
+	{
+		int a = 100;
+		
+		
+		while (a<=200)
+		{
+			if (a%10 ==3 || a%10 ==6){
+
+				System.out.println(a);
+			}
+			a++;
+		}
+
+
+	}
+}
