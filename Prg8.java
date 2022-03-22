34eaf0a2a5d296e7e77a0ca18258eb81d81e35e0
gudugudugud <102131981+gudugudugud@users.noreply.github.com>
2022-03-22 10:00:09 +0530

    Add files via upload

---
+++ Prg8.java
@@ -0,0 +1,22 @@
+class Prg8 
+{
+	public static void main(String[] args) 
+	{
+	int a = 21;
+
+	switch (a%2)
+	{
+	case 0:
+		
+		
+		System.out.println("it is a even number");
+		break;
+		
+	case 1:
+		
+			System.out.println ("it is a odd number");
+		
+	
+	}
+	}
+}
