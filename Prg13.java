6fb8b614b428d0a4e7167545236018a2b9d453e8
gudugudugud <102131981+gudugudugud@users.noreply.github.com>
2022-03-22 10:16:16 +0530

    Add files via upload

---
+++ Prg13.java
@@ -0,0 +1,24 @@
+class Prg13 
+{
+	public static void main(String[] args) 
+	{
+		int a = 23487;
+		int count = 0;
+		
+		while (a>0)
+		{
+			
+				if (a%2==0)
+				{
+					count ++;
+				}
+					
+				
+				a=a/10;
+				
+		}
+		
+System.out.println (count);
+
+	}
+}
