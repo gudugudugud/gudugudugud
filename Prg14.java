5c6731a44fda839e8a4ea3747776f36cbcc52105
gudugudugud <102131981+gudugudugud@users.noreply.github.com>
2022-03-22 10:17:04 +0530

    Add files via upload

---
+++ Prg14.java
@@ -0,0 +1,18 @@
+class Prg14 
+{
+	public static void main(String[] args) 
+	{
+	int a= 23487;
+	while (a>0)
+	{
+		int b = a%10;
+		if (b%2==1)
+		{
+			System.out.println (b);
+		}
+		//int b = a%10;
+//System.out.println (b);
+a = a/10;
+	}
+	}
+}
